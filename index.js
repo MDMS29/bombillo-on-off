d6c6f38c7c4b55b91daeb6a31c5195491e2a8c02
MDMS29 <103197915+MDMS29@users.noreply.github.com>
2022-10-04 10:23:18 -0500

    Cambios de clase auto

--- index.js
+++ index.js
@@ -3,21 +3,24 @@ document.addEventListener('pointerenter', () =>{
     
     const on = document.querySelector('.on')
     on.addEventListener("click", () => {
+        const main = document.querySelector('main')
+        main.classList.replace('desactivado', 'active')
         const titulo = document.querySelector('.titulo');
         titulo.textContent = 'Encendido';
         contenedor_bombillo.innerHTML = `
-            <div class="contenedor d-flex gap-4 flex-column">
-                <div class="contenedor-img">
-                    <img src="assets/encendido.png" alt="bombillo encendido">
-                </div>
-                <div class="d-flex justify-content-center">
-                    <button class="off btn btn-dark">Off</button>
-                </div>
+        <div class="contenedor d-flex gap-4 flex-column">
+            <div class="contenedor-img">
+                <img src="assets/encendido.png" alt="bombillo encendido">
             </div>
-            
+            <div class="d-flex justify-content-center">
+                <button class="off btn btn-dark">Off</button>
+            </div>
+        </div>
+        
         `
         const off = document.querySelector('.off')
         off.addEventListener('click', () => {
+            main.classList.replace('active', 'desactivado')
             const titulo = document.querySelector('.titulo');
             titulo.textContent = 'Apagado';
             contenedor_bombillo.innerHTML = `
