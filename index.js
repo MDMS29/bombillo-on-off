a1d9e5f66c7d4ae0ec35a21017a087fde790a4bb
MDMS29 <mdmazo30@misena.edu.co>
2022-10-03 20:43:57 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,35 @@
+document.addEventListener('pointerenter', () =>{
+    const contenedor_bombillo = document.querySelector('.contenedor')
+    
+    const on = document.querySelector('.on')
+    on.addEventListener("click", () => {
+        const titulo = document.querySelector('.titulo');
+        titulo.textContent = 'Encendido';
+        contenedor_bombillo.innerHTML = `
+            <div class="contenedor d-flex gap-4 flex-column">
+                <div class="contenedor-img">
+                    <img src="assets/encendido.png" alt="bombillo encendido">
+                </div>
+                <div class="d-flex justify-content-center">
+                    <button class="off btn btn-dark">Off</button>
+                </div>
+            </div>
+            
+        `
+        const off = document.querySelector('.off')
+        off.addEventListener('click', () => {
+            const titulo = document.querySelector('.titulo');
+            titulo.textContent = 'Apagado';
+            contenedor_bombillo.innerHTML = `
+            <div class="contenedor d-flex gap-4 flex-column">
+                <div class="contenedor-img">
+                    <img src="assets/apagado.png" alt="">
+                </div>
+                <div class=" d-flex justify-content-center">
+                    <button class="on btn btn-warning">On</button>
+                </div>
+            </div>
+            `
+        })
+    })
+})
